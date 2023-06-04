76ab1d8d02e86946f8f7bbff0b81ce53874b9fe3
Abdul Rahman Al Sbeinaty <61135058+vladiusftw@users.noreply.github.com>
2023-06-04 11:23:52 +0400

    register route

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import fastifyEnv from "@fastify/env";
 import Fastify from "fastify";
 import mongoose from "mongoose";
+import cors from "@fastify/cors";
 import UsersRoutes from "./users/routes.js";
+import fastifyJwt from "@fastify/jwt";
 
 const fastify = Fastify({
   logger: true,
@@ -9,7 +11,14 @@ const fastify = Fastify({
 
 const envSchema = {
   type: "object",
-  required: ["PORT", "MONGO_USERNAME", "MONGO_PASSWORD", "DB_NAME"],
+  required: [
+    "PORT",
+    "MONGO_USERNAME",
+    "MONGO_PASSWORD",
+    "DB_NAME",
+    "FRONTEND_URL",
+    "SUPER_SECRET",
+  ],
   properties: {
     PORT: {
       type: "string",
@@ -24,6 +33,13 @@ const envSchema = {
     DB_NAME: {
       type: "string",
     },
+    FRONTEND_URL: {
+      type: "string",
+      default: "https://localhost",
+    },
+    SUPER_SECRET: {
+      type: "string",
+    },
   },
 };
 const options = {
@@ -33,6 +49,12 @@ const options = {
 
 await fastify.register(fastifyEnv, options);
 
+await fastify.register(cors, {
+  origin: fastify.config.FRONTEND_URL,
+});
+
+await fastify.register(fastifyJwt, { secret: fastify.config.SUPER_SECRET });
+
 try {
   const url = `mongodb+srv://${fastify.config.MONGO_USERNAME}:${fastify.config.MONGO_PASSWORD}@cluster0.f0kpkbh.mongodb.net/${fastify.config.DB_NAME}?retryWrites=true&w=majority`;
   await mongoose.connect(url);
@@ -41,7 +63,7 @@ try {
   console.log(e);
 }
 
-await fastify.register(UsersRoutes);
+await fastify.register(UsersRoutes, { prefix: "/api" });
 
 // Run the server!
 fastify.listen({ port: fastify.config.PORT }, function (err, address) {
@@ -51,3 +73,5 @@ fastify.listen({ port: fastify.config.PORT }, function (err, address) {
   }
   // Server is now listening on ${address}
 });
+
+export { fastify };
---
+++ users/fastifySchema.js
@@ -0,0 +1,26 @@
+const bodyRegisterUserSchema = {
+  type: "object",
+  required: ["name", "dob", "phone", "email", "occupation", "nationality"],
+  properties: {
+    name: {
+      type: "string",
+    },
+    dob: {
+      type: "string",
+    },
+    phone: {
+      type: "number",
+    },
+    email: {
+      type: "string",
+    },
+    occupation: {
+      type: "string",
+    },
+    nationality: {
+      type: "string",
+    },
+  },
+};
+
+export { bodyRegisterUserSchema };
